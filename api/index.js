6ff1d08141af17b07b928fc45e0ff825aa6aa928
JCPrime01 <vanzokiller@gmail.com>
2026-06-03 11:58:41 -0300

    chore: shareCode ZDFG651R8QK -> MAU2EYH014B (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/glq672bl?shareCode=ZDFG651R8QK&afp=oa' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/glq672bl?shareCode=MAU2EYH014B&afp=oa' });
     res.end();
   }
